083bf49f42aa69e822fff6c7369c97e862b9b9bf
hakandereli <hakandereli28@gmail.com>
2023-12-17 18:51:57 +0300

    java loops 2 problem solved

---
+++ src/main/java/org/java/JavaLoopsII.java
@@ -0,0 +1,31 @@
+package org.java;
+
+import java.util.Scanner;
+
+public class JavaLoopsII {
+
+    //Problem URL : https://www.hackerrank.com/challenges/java-loops/problem
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int t = in.nextInt();
+
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int calc = a;
+            for(int j=0;j<n;j++)
+            {
+                calc+=(Math.pow(2,j)*b);
+                System.out.print(calc+" ");
+            }
+            System.out.println();
+        }
+
+
+        in.close();
+    }
+}
