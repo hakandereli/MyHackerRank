8e741b82bcbfe49c6b13571284adcc953f71c055
hakandereli <hakandereli28@gmail.com>
2023-12-17 19:58:02 +0300

    basic select - salary-of-employees

--- src/main/java/org/sql/basic_select.sql
+++ src/main/java/org/sql/basic_select.sql
@@ -1,5 +1,6 @@
 --Problem URL : https://www.hackerrank.com/challenges/name-of-employees/problem
-
 select name from Employee order by name asc;
 
 
+--Problem URL : https://www.hackerrank.com/challenges/salary-of-employees/problem
+select name from Employee where salary > 2000 and months < 10 order by employee_id;
