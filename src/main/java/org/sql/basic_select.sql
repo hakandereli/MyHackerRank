f4805b9d1878e6190ded3d2ee54b60497e877883
hakandereli <hakandereli28@gmail.com>
2023-12-19 23:42:15 +0300

    basic select - count example

--- src/main/java/org/sql/basic_select.sql
+++ src/main/java/org/sql/basic_select.sql
@@ -4,3 +4,6 @@ select name from Employee order by name asc;
 
 --Problem URL : https://www.hackerrank.com/challenges/salary-of-employees/problem
 select name from Employee where salary > 2000 and months < 10 order by employee_id;
+
+--Problem URL : https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
+select count(name) from city where population > 100000;
