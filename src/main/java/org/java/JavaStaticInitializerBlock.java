4faec4ad0ebd2e786d4ecbf0cfabbb02f298a3a0
hakandereli <hakandereli28@gmail.com>
2023-12-26 23:47:24 +0300

    Java Static Initializer Block

---
+++ src/main/java/org/java/JavaStaticInitializerBlock.java
@@ -0,0 +1,30 @@
+package org.java;
+
+import java.util.Scanner;
+
+public class JavaStaticInitializerBlock {
+
+//    problem url : https://www.hackerrank.com/challenges/java-static-initializer-block/problem
+
+    static int B, H;
+    static boolean flag;
+
+    static {
+        Scanner cin = new Scanner(System.in);
+        B = cin.nextInt();
+        H = cin.nextInt();
+        if (B > 0 && H > 0) {
+            flag = true;
+        } else {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+
+    public static void main(String[] args) {
+        if (flag) {
+            int area = B * H;
+            System.out.println(area);
+        }
+    }
+
+}
