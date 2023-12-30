6c1547ea57d49f9560e8390b666ab3ddc24921c2
hakandereli <hakandereli28@gmail.com>
2023-12-30 21:55:44 +0300

    basic join - average-population-of-each-continent

--- src/main/java/org/sql/basic_join.sql
+++ src/main/java/org/sql/basic_join.sql
@@ -2,3 +2,9 @@
 SELECT City.Name
 FROM City, Country
 WHERE City.CountryCode = Country.Code AND Country.Continent  = 'Africa';
+
+-- problem url:hackerrank.com/challenges/average-population-of-each-continent/problem
+select Country.Continent , FLOOR(AVG(City.Population))
+FROM City, Country
+WHERE City.CountryCode = Country.Code
+GROUP BY Country.Continent;
