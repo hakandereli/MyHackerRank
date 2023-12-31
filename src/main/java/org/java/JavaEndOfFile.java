9f953b42e9a0072aee000a57a005a4e8989581fd
hakandereli <hakandereli28@gmail.com>
2023-12-31 23:18:13 +0300

    java end of file problem

---
+++ src/main/java/org/java/JavaEndOfFile.java
@@ -0,0 +1,18 @@
+package org.java;
+
+import java.util.Scanner;
+
+public class JavaEndOfFile {
+
+//    problem url: https://www.hackerrank.com/challenges/java-end-of-file/problem
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i=1;
+        while (sc.hasNext()) {
+            String s=sc.nextLine();
+            System.out.println(i + " " + s);
+            i++;
+        }
+    }
+}
