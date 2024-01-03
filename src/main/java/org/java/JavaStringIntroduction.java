b7c9bcb68c3108aac69c44b1cd2e5101a09cb436
hakandereli <hakandereli28@gmail.com>
2024-01-03 20:58:48 +0300

    JavaStringIntroduction

---
+++ src/main/java/org/java/JavaStringIntroduction.java
@@ -0,0 +1,32 @@
+package org.java;
+
+import java.util.Scanner;
+
+public class JavaStringIntroduction {
+
+//    problem url: https://www.hackerrank.com/challenges/java-strings-introduction/problem
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        System.out.println(A.length() + B.length());
+        if (A.compareTo(B) > 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+        A = (A.substring(0, 1)).toUpperCase() + A.substring(1);
+        B = (B.substring(0, 1)).toUpperCase() + B.substring(1);
+        System.out.println(A + " " + B);
+
+    }
+
+
+}
+
+
